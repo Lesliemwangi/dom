0ddc0b34611e663ed6724c476eb891ed39caa9b8
Leslie Mwangi <luciannahphoi@gmail.com>
2024-03-28 12:34:11 +0300

    dom

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("Our js is linked to the html");
+
+//   const header = document.querySelector("h1");
+
+//   const paragraph = document.getElementById("my-paragraph");
+//   paragraph.innerText = "New text";
+
+//   console.log(paragraph);
+// });
+
+console.log("Our js is linked to the html");
+
+const header = document.querySelector("h1");
+
+const paragraph = document.getElementById("my-paragraph");
+paragraph.innerText = "New text";
+
+console.log(paragraph);
